4edffbe7984c76077c24cc0b76120a83d5593266
Rivernina <rivernina512@gmail.com>
2018-10-02 22:19:04 -0500

    Lesson00-Prework
    Prework Lesson 00

---
+++ Lesson00-Prework/Lesson00-Prework/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lesson00_Prework.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
