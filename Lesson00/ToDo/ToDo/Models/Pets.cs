9b087b2592e0a961f15a8a383b244f5f4556a82e
Rivernina <rivernina512@gmail.com>
2018-10-11 11:03:16 -0500

    Lesson00 Homework
    Created a new layout _MyLayout which is loaded during a minute of the day that is divisible by 2

---
+++ Lesson00/ToDo/ToDo/Models/Pets.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDo.Models
+{
+    public class Pets
+    {
+        public int PetsNumber { get; set; }
+        public string PetsType { get; set; }
+
+        public string PetsColor { get; set; }
+
+        public string PetsName { get; set; }
+
+        public DateTime CurrentTime { get; set; }
+
+    }
+}
